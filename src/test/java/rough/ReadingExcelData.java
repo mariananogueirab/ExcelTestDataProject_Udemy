6ab966654cfd3c381cd5829b6d59db56a8c08d23
mariananogueirab <mariananbp@gmail.com>
2022-08-23 18:43:20 -0300

    checking how many rows are in the test case

--- src/test/java/rough/ReadingExcelData.java
+++ src/test/java/rough/ReadingExcelData.java
@@ -7,20 +7,30 @@ public class ReadingExcelData {
 
     public static void main(String[] args) {
         ExcelReader excel = new ExcelReader("src/test/resources/testdata/BankManagerSuite.xlsx");
-        int rows = excel.getRowCount(Constants.DATA_SHEET);
-        System.out.println("Number of rows: " + rows);
+        int numberOfRowsSheet = excel.getRowCount(Constants.DATA_SHEET);
+        System.out.println("Number of rows in the sheet: " + numberOfRowsSheet);
 
         String testName = "AddCustomerTest";
 
+        // Find the test case start row
         int testCaseRowNum = 1;
 
-        for(testCaseRowNum=1; testCaseRowNum <= rows; testCaseRowNum++) {
+        for(testCaseRowNum=1; testCaseRowNum <= numberOfRowsSheet; testCaseRowNum++) {
 
             String testCaseName = excel.getCellData(Constants.DATA_SHEET, 0, testCaseRowNum);
 
             if (testCaseName.equalsIgnoreCase(testName)) break;
         }
 
-        System.out.println("Test case starts from row num: " +testCaseRowNum);
+        // Checking total rows in test case
+        int dataStartRowNum = testCaseRowNum + 2; // to jump the head line
+        System.out.println("Data starts row num: " + dataStartRowNum);
+
+        int testRows = 0;
+        // Enquanto a célula não for branca, vai incrementar
+        while (!excel.getCellData(Constants.DATA_SHEET, 0, dataStartRowNum+testRows).isBlank()) {
+            testRows++;
+        }
+        System.out.println("Total of rows in test case: " + testRows);
     }
 }
