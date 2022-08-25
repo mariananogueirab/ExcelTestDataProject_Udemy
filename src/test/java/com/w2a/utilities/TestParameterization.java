a201038ee63f5705b5d3cef9d0f6b0c82247ec8c
mariananogueirab <mariananbp@gmail.com>
2022-08-24 21:57:26 -0300

    complex parameterization with Hashtable

--- src/test/java/com/w2a/utilities/TestParameterization.java
+++ src/test/java/com/w2a/utilities/TestParameterization.java
@@ -3,12 +3,15 @@ package com.w2a.utilities;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.Hashtable;
+
 public class TestParameterization {
 
     @Test(dataProvider = "getData")
-    public void testData(String runmode, String firstname, String lastname, String postcode) {
-
-        System.out.println(runmode+"---"+firstname+"----"+lastname+"--"+postcode);
+    // public void testData(String runmode, String firstname, String lastname, String postcode) {
+    public void testData(Hashtable<String, String> data) {
+        // System.out.println(data.get("Runmode")+"---"+data.get("firstname")+"----"+data.get("lastname")+"--"+data.get("postcode"));
+        System.out.println(data);
     }
 
     @DataProvider
@@ -18,15 +21,13 @@ public class TestParameterization {
         int numberOfRowsSheet = excel.getRowCount(Constants.DATA_SHEET);
         // System.out.println("Number of rows in the sheet: " + numberOfRowsSheet);
 
-        String testName = "AddCustomerTest";
+        String testName = "OpenAccountTest";
 
         // Find the test case start row
         int testCaseRowNum = 1;
 
         for(testCaseRowNum=1; testCaseRowNum <= numberOfRowsSheet; testCaseRowNum++) {
-
             String testCaseName = excel.getCellData(Constants.DATA_SHEET, 0, testCaseRowNum);
-
             if (testCaseName.equalsIgnoreCase(testName)) break;
         }
 
@@ -44,8 +45,9 @@ public class TestParameterization {
         // Checking total of columns in test case
 
         int testCols = 0;
+        int colStartColNum = testCaseRowNum +1;
 
-        while(!excel.getCellData(Constants.DATA_SHEET, testCols, dataStartRowNum).isBlank()) {
+        while(!excel.getCellData(Constants.DATA_SHEET, testCols, colStartColNum).isBlank()) {
             testCols++;
         }
 
@@ -53,13 +55,26 @@ public class TestParameterization {
 
         // Printing data
 
-        Object[][] data = new Object[testRows][testCols];
+        Object[][] data = new Object[testRows][1]; // cols = 1 because of the hashdata
 
+        int incrementTable = 0;
         for(int rowNum = dataStartRowNum; rowNum < (dataStartRowNum + testRows); rowNum++) {
+            Hashtable<String, String> table = new Hashtable<>(); // cria uma hashtable pra cada row
+            // Uma Hashtable é <Key, Value>
             for(int colNum = 0; colNum < testCols; colNum++) {
                 // System.out.println(excel.getCellData(Constants.DATA_SHEET, colNum, rowNum));
-                data[rowNum-dataStartRowNum][colNum] = excel.getCellData(Constants.DATA_SHEET, colNum, rowNum);
+                String testData = excel.getCellData(Constants.DATA_SHEET, colNum, rowNum); // pega os dados
+                String colName = excel.getCellData(Constants.DATA_SHEET, colNum, colStartColNum); // pega o cabeçalho
+
+                table.put(colName, testData);
+                // System.out.println(table);
+                // Para cada coluna que eu rodar, vai ter uma chave (key, value) na table, onte a key vai ser o nome da coluna e o value o dado
+                // {postcode=A234324, Runmode=Y, lastname=Potter, firstname=Harry}
+                // cada vez que rodar vai colocar um par de chave e valor desse pra row até completar todas as colunas, ai vai pro for de cima pra formar uma nova row
             }
+            // coloca a linha da tabela que formou toda na mesma coluna de data
+            data[incrementTable][0] = table;
+            incrementTable++;
         }
 
         return data;
